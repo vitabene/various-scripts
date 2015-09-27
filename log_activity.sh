4a356ca3d05413ca63cafbf89e031e66cb291512
Vitezslav Benes <vitezslav.benes@gmail.com>
2015-09-27 20:03:10 +0200

    Added example log file in .jsonl format for easier data extraction

--- log_activity.sh
+++ log_activity.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-function getWindowTitle {
+LOGPATH=./activity_log.jsonl
+# JSON date format
+DATEFORMAT="%Y-%m-%dT%TZ"
+function recordWindowTitle {
 	# getting id of a process running in currently focused window
 	PID=$(xprop -id `xdotool getwindowfocus` | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
 	# extracting a name of the focused window process
 	PNAME=$(ps -p $PID -o comm=)
 	# appending date, name of the active application and a number of times
 	# that the loop has run since the script was run
-	echo "["$(date +"%d-%m-%y_%T")"]["$PNAME"][$run_times]">>~/activity_log/activity_log.txt
+	DATASTRING="["\"$(date +"$DATEFORMAT")"\",\""$PNAME"\", \"$run_times\"]"
+	echo $DATASTRING>>$LOGPATH
 }
 # a number of times the while loop has run
 run_times=0
+# create log file if one does not exist
+if [ ! -f $LOGPATH ]
+  then
+    CELLDEFINITION="[\"Date\", \"Application Name\", \"Loop #\"]"
+		echo $CELLDEFINITION>>$LOGPATH
+fi
 while true
 do
-	getWindowTitle
-	sleep 1m
+	recordWindowTitle
+	sleep 2m
 	((run_times++))
 done
